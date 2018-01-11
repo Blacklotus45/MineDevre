e34a442621cf5b9186d2a4f4dee615c090aad4b5
TolunayGedikoğlu <tolunaygedikoglu@gmail.com>
2018-01-12 02:15:52 +0300

    Main menu added
    *Main menu added.
    Current menu items:
    -Topics
    -About
    -Exit
    
    *TextMesh Pro asset is also added to project. (for better looking texts)

---
+++ Assets/TopicsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TopicsMenu : MonoBehaviour {
+
+    public void startDcCircuitsScene()
+    {
+        SceneManager.LoadScene("DcCircuits");
+    }
+
+}
