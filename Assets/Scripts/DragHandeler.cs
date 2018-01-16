c29dbcbe75b66235095c351254c943f8bc5438a6
Berhan Can Gündoğdu <bcgundogdu@gmail.com>
2018-01-16 14:42:37 +0300

    Camera Oriented, Spawn Calculations made, some minor adjustments, DragNDrop in it's simple form

--- Assets/Scripts/DragHandeler.cs
+++ Assets/Scripts/DragHandeler.cs
@@ -3,28 +3,112 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Image))]
 public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
-    
+
+	public enum ElementType
+    {
+    	Wire,
+    	Resistance,
+    	Battery,
+    	Switch,
+    	Lamp
+    }
+
+	static public DragHandeler draggedItem;
+    static public GameObject icon;
+    static public GameObject[] CircuitElements;
+
+	public ElementType typeOfItem;
+
+	public delegate void DragEvent(DragHandeler item);
+    static public event DragEvent OnItemDragStartEvent;                             // Drag start event
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
-	
+
+        draggedItem = this;                                                         // Set as dragged item
+        icon = new GameObject("Icon");                                              // Create object for item's icon
+        Image image = icon.AddComponent<Image>();
+        image.sprite = GetComponent<Image>().sprite;
+        image.raycastTarget = false;                                                // Disable icon's raycast for correct drop handling
+        RectTransform iconRect = icon.GetComponent<RectTransform>();
+        // Set icon's dimensions
+        iconRect.sizeDelta = new Vector2(   GetComponent<RectTransform>().sizeDelta.x,
+                                            GetComponent<RectTransform>().sizeDelta.y);
+        Canvas canvas = GetComponentInParent<Canvas>();                             // Get parent canvas
+        if (canvas != null)
+        {
+            // Display on top of all GUI (in parent canvas)
+            icon.transform.SetParent(canvas.transform, true);                       // Set canvas as parent
+            icon.transform.SetAsLastSibling();                                      // Set as last child in canvas transform
+        }
+        if (OnItemDragStartEvent != null)
+        {
+            OnItemDragStartEvent(this);                                             // Notify all about item drag start
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+//        Debug.Log("OnDrag");
 
-        this.transform.position = eventData.position;
-
-
-
+		if (icon != null)
+        {
+            icon.transform.position = Input.mousePosition;                          // Item's icon follows to cursor
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
 
+		if (icon != null)
+        {
+            Destroy(icon);                                                          // Destroy icon on item drop
+        }
+//        MakeVisible(true);                                                          // Make item visible in cell
+        {
+        	GameObject go;
+        	switch (draggedItem.typeOfItem) {
+        		case ElementType.Wire:
+        			go = Instantiate(ElementList.TheList.RcElements[0]);
+					break;
+				case ElementType.Resistance:
+					go = Instantiate(ElementList.TheList.RcElements[1]);
+					break;
+				case ElementType.Battery:
+					go = Instantiate(ElementList.TheList.RcElements[2]);
+					break;
+				case ElementType.Switch:
+					go = Instantiate(ElementList.TheList.RcElements[3]);
+					break;
+				case ElementType.Lamp:
+					go = Instantiate(ElementList.TheList.RcElements[4]);
+					break;
+        		default:
+        			Debug.LogWarning("ElementType not initialized");
+        			go = new GameObject();
+        			break;
+        	}
+
+			go.transform.position = spawnPosition();
+        }
+        draggedItem = null;
+        icon = null;
+    }
+
+    private Vector3 spawnPosition()
+    {
+		float orthoSize = Camera.main.orthographicSize;
+		float aspectRatio = Camera.main.aspect;
+    	float verticalUnitperPixel = orthoSize*2 / Camera.main.pixelHeight;
+		float horizontalUnitperPixel = (orthoSize*2 * aspectRatio) 
+										/ Camera.main.pixelWidth;
+		float spawnX = Input.mousePosition.x * horizontalUnitperPixel - (orthoSize * aspectRatio);
+		float spawnY = Input.mousePosition.y * verticalUnitperPixel - orthoSize;
+		return new Vector3(spawnX, spawnY, 20);
     }
 
 }
---
+++ Assets/Scripts/ElementList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementList : MonoBehaviour {
+
+	static public ElementList TheList;
+
+	public GameObject[] RcElements;
+
+	// Use this for initialization
+	void Start () {
+		if (TheList == null)
+		{
+			TheList = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+}
