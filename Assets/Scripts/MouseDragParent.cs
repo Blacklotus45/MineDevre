bb47cc4b7de1dbc78775f9d680f4256d49fbe825
Berhan Can Gündoğdu <bcgundogdu@gmail.com>
2018-01-28 19:21:52 +0300

    Wire prefab updated for our needs, sprite sheet modified a little, Drag parent script added

---
+++ Assets/Scripts/MouseDragParent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDragParent : MonoBehaviour {
+
+	float distance = 20;
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+        transform.parent.position = objPosition;
+
+    }
+}
