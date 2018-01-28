3fcda5bc81bc34d0221ab74b45011a618fb83d62
Berhan Can Gündoğdu <bcgundogdu@gmail.com>
2018-01-28 13:18:24 +0300

    CricuitElement added for connecting the circuit, it's edges are abstractly added, prefabs updated to accordingly, background size increased + z dept increased to eliminate collusion

---
+++ Assets/Scripts/CircuitElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitElement : MonoBehaviour {
+
+	public enum ElementType
+    {
+    	Wire,
+    	Resistance,
+    	Battery,
+    	Switch,
+    	Lamp
+    }
+
+	public ElementType typeOfItem;
+
+	public CircuitElement rightSide = null;
+	public CircuitElement leftSide = null;
+
+	public void ConnectToLeft(GameObject other)
+	{
+		leftSide = other.GetComponent<CircuitElement>();
+	}
+
+	public void ConnectToRight(GameObject other)
+	{
+		rightSide = other.GetComponent<CircuitElement>();
+	}
+	
+
+}
---
+++ Assets/Scripts/EdgeHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeHandler : MonoBehaviour {
+
+	public bool LeftEdge;
+
+	public void ConnectToParent (GameObject otherElement)
+	{
+		CircuitElement parent = gameObject.GetComponentInParent<CircuitElement> ();
+		if (parent != null)
+		{
+			if (LeftEdge)
+			{
+				parent.ConnectToLeft (otherElement);
+			}
+			else 
+			{
+			  parent.ConnectToRight (otherElement);
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Found an edge circle without parent<CircuitElement>");
+		}
+
+	}
+}
