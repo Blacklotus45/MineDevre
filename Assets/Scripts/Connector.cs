410075d7be512f960e73b0918dd0070b0f972772
Cemil Ulas Dalgic <cemil.dalgic@gmail.com>
2018-01-30 15:17:59 +0300

    Connector Game Object Created and LinkedList Script Created

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour {
+
+	public static LinkedList<GameObject> ItemList = new LinkedList<GameObject>();
+
+}
