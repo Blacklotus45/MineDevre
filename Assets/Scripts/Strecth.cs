aae167f08157d836dcf6e0fdbae8f6dc3c8b7faf
Berhan-PC\Berhan <bcgundogdu@gmail.com>
2018-02-08 17:53:54 +0300

    Tanjant bug fix and revised strecth code. No visual glitch for now.

--- Assets/Scripts/Strecth.cs
+++ Assets/Scripts/Strecth.cs
@@ -22,29 +22,13 @@ public class Strecth : MonoBehaviour {
 
 	void OnMouseDrag()
     {
-//		GetComponent<CircleCollider2D>().enabled = false;
     	
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30f);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition) ;
 
         transform.position = objPosition;
 
-//        Debug.Log(objPosition + " Anchor " + anchor.position);
-
-        //Quaternion.LookRotation(Vector3.forward,Vector3.up);
-		float lengthOfWire = Vector3.Magnitude(objPosition - anchor.position);
-        float rotation = Mathf.Atan((objPosition.y - anchor.position.y) / (objPosition.x - anchor.position.x)) / 2;
-
-        middle.position = (transform.position + anchor.position) / 2;
-
-		Vector3 scale = middle.localScale;
-		scale.x = lengthOfWire - (1.836f * 0.3f);
-        middle.localScale = scale;
-
-        Quaternion newRot = middle.rotation;
-        newRot.z = rotation;
-        middle.rotation = newRot;
-
+		StrecthTo (objPosition);
     }
 
     public void StrecthTo(Vector3 lastPos)
@@ -58,8 +42,8 @@ public class Strecth : MonoBehaviour {
 		scale.x = lengthOfWire - (1.836f * 0.3f);
         middle.localScale = scale;
 
-        Quaternion newRot = middle.rotation;
-        newRot.z = rotation;
-        middle.rotation = newRot;
+		Vector3 newRot = middle.eulerAngles;
+		newRot.z = rotation * Mathf.Rad2Deg * 2;
+		middle.eulerAngles = newRot;
     }
 }
