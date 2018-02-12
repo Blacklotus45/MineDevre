6d40acd92f7f38a4e098d89d790abe817dc008d3
Cemil Ulas Dalgic <cemil.dalgic@gmail.com>
2018-02-12 21:57:42 +0300

    Parametre Girişi Sağlandı

---
+++ Assets/Scripts/AddValue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddValue : MonoBehaviour {
+
+	public GameObject selectedElement;
+
+	public string voltageValue;
+	public string currentValue;
+
+	public InputField voltageField;
+	public InputField currentField;
+
+	public void Start () {
+
+		voltageField.text = "Enter Voltage Here...";
+		currentField.text = "Enter Current Here...";
+	
+	}
+
+	public void Awake(){
+	
+	}
+
+	public void GetInput(){
+
+		voltageValue = voltageField.text;
+		currentValue = currentField.text;
+		int.Parse (voltageValue);
+		int.Parse (currentValue);
+	}
+
+}
