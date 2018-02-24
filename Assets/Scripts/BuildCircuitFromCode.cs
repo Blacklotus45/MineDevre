b51ffc2a3f977d1b38c12f804a7f35849eab69a1
alperenboyaci <alperen.boyaci@hotmail.com>
2018-02-24 13:42:29 +0300

    Devre elemanlarının oluşturulması için gerekli kod giriş alanının oluşturulması

---
+++ Assets/Scripts/BuildCircuitFromCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildCircuitFromCode : MonoBehaviour {
+	public Button myButton;
+	public InputField circuit_code;
+
+	void Start () {
+		Button btn = myButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("The code entered : "+circuit_code.text);
+	}
+
+}
