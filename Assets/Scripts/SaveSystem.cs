cc2d73aafdc98711e02d4300981260803fb67c47
Berhan Can Gündoğdu <bcgundogdu@gmail.com>
2018-03-21 17:10:41 +0300

    checkpoint

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour {
+
+	public static SaveSystem instance;
+	public string saveCode;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (this);
+		}
+	}
+
+	public void Save(int slot){
+		string tmp = "save" + slot;
+		PlayerPrefs.SetString (tmp, saveCode);
+	}
+	
+	public void Load(int slot){
+		string tmp = PlayerPrefs.GetString ("save"+slot);
+		GameObject.FindObjectOfType<BuildCircuitFromCode> ().TaskOnClick(tmp);
+
+	}
+}
