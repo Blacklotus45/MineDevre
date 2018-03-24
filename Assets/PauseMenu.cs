faf8807eb622db771743662e781348544e65ffa6
TolunayGedikoğlu <tolunaygedikoglu@gmail.com>
2018-03-25 00:24:30 +0300

    Return to menu and quit options are added.
    Using the arrow button in top left corner, menu can be open.
    In PC, ESC key also brings up the menu.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+    }
+
+    public void LoadMenuScene()
+    {
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+
+
+
+
+
+
+}
